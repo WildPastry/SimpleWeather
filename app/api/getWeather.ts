ff25fdcc6951c5c1bea3cf8f66b105baf19358b9
WildPastry <mparkernzdev@gmail.com>
2024-01-27 21:09:08 +1300

    Refactor

--- app/api/getWeather.ts
+++ app/api/getWeather.ts
@@ -14,7 +14,7 @@ const getWeather = async (): Promise<IWeather> => {
       `${apiUrl + currentLat}&lon=${currentLng}&units=metric&APPID=${apiKey}`,
     );
     const responseJSON = await response.json();
-    
+
     const weatherData: IWeather = {
       temp: Math.round(responseJSON.current.temp),
       high: Math.round(responseJSON.daily[0].temp.max),
@@ -30,64 +30,12 @@ const getWeather = async (): Promise<IWeather> => {
       hourly: responseJSON.hourly,
       daily: responseJSON.daily,
     };
-    
-    console.log(weatherData);
     return weatherData;
   } catch (error) {
     console.log('getWeather', error);
-    throw error; // Throw the error to be caught in the setData function
+    throw error;
   }
 
-
-
-  // const [weatherData, setWeatherData] = useState<IWeather>({
-  //   temp: 0,
-  //   high: 0,
-  //   low: 0,
-  //   openWeatherId: "",
-  //   desc: "",
-  //   humidity: "",
-  //   wind: 0,
-  //   icon: "",
-  //   sunset: "",
-  //   sunrise: "",
-  //   feelsLike: "",
-  //   hourly: "",
-  //   daily: "",
-  // });
-
-  // // Fetch data
-  // Promise.all([
-  //   fetch(
-  //     `${apiUrl + currentLat}&lon=${currentLng}&units=metric&APPID=${apiKey}`,
-  //   ),
-  // ])
-  //   .then(([response]) => {
-  //     // Convert response to JSON
-  //     return Promise.all([response.json()]);
-  //   })
-  //   .then(([responseJSON]) => {
-  //     // Set response locally
-  //     const weatherData: IWeather = {
-  //       temp: Math.round(responseJSON.current.temp),
-  //       high: Math.round(responseJSON.daily[0].temp.max),
-  //       low: Math.round(responseJSON.daily[0].temp.min),
-  //       openWeatherId: responseJSON.current.weather[0].id,
-  //       desc: responseJSON.current.weather[0].description,
-  //       humidity: responseJSON.current.humidity,
-  //       wind: responseJSON.current.wind_speed * 3.6,
-  //       icon: responseJSON.current.weather[0].icon,
-  //       sunset: responseJSON.current.sunset,
-  //       sunrise: responseJSON.current.sunrise,
-  //       feelsLike: responseJSON.current.feels_like,
-  //       hourly: responseJSON.hourly,
-  //       daily: responseJSON.daily,
-  //     };
-  //     setWeatherData(weatherData);
-  //   });
-
-  //   console.log(weatherData)
-  // return weatherData;
 };
 
 export default getWeather;
