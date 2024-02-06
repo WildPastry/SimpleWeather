24e036f423cc6585d7c7baf8a7bbf08097c84e0c
WildPastry <mparkernzdev@gmail.com>
2024-02-06 16:27:31 +1300

    Add new env

--- app/screens/home/components/Header.tsx
+++ app/screens/home/components/Header.tsx
@@ -1,9 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 const Header: React.FC = (): JSX.Element => {
+  const appName: string | undefined = process.env.EXPO_PUBLIC_APP_NAME;
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Header</Text>
+      <Text style={styles.title}>{appName}</Text>
     </View>
   );
 };
