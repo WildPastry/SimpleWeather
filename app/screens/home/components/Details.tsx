1afb19adbfa77b4c01e912f22bc18d3ded208f2e
WildPastry <mparkernzdev@gmail.com>
2024-02-10 19:26:08 +1300

    Add icons

--- app/screens/home/components/Details.tsx
+++ app/screens/home/components/Details.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable max-len */
 import { StyleSheet, Text, View } from 'react-native';
 import { IHourly } from '../../../types/data.types';
+import { Ionicons } from '@expo/vector-icons';
 import colours from '../../../assets/colours.json';
+import weatherIcons from '../../../assets/icons.json';
 
 interface IDetails {
   desc: string;
@@ -16,9 +19,37 @@ const Details: React.FC<IDetails> = (props: IDetails): JSX.Element => {
   return (
     <View style={styles.container}>
       {/* Icons */}
+      <View style={styles.iconsWrapper}>
+        {/* Morning */}
+        <View style={styles.weatherIconWrapper}>
+          <Text style={styles.weatherIcon}>
+            {/* 9am */}
+            {weatherIcons[props.hourly[9].weather[0].id].code}
+          </Text>
+          <Text style={styles.infoHeading}>7am</Text>
+        </View>
+        {/* Afternoon */}
+        <View style={styles.weatherIconWrapper}>
+          <Text style={styles.weatherIcon}>
+            {/* 12pm */}
+            {weatherIcons[props.hourly[14].weather[0].id].code}
+          </Text>
+          <Text style={styles.infoHeading}>mid-day</Text>
+        </View>
+        <View style={styles.weatherIconWrapper}>
+          {/* Evening */}
+          <Text style={styles.weatherIcon}>
+            {/* 6pm */}
+            {weatherIcons[props.hourly[19].weather[0].id].code}
+          </Text>
+          <Text style={styles.infoHeading}>5pm</Text>
+        </View>
+      </View>
       {/* Description */}
       <View style={styles.descWrapper}>
-        <Text style={styles.descTitle}>Daily Summary</Text>
+        <Text style={styles.descTitle}>
+          <Ionicons name='time' size={19} color={colours.white} /> Daily Summary
+        </Text>
         <Text style={styles.descSummary}>
           Feels like {Math.round(props.feelsLike)}°, {props.desc} with{' '}
           {Math.round(props.wind)} km/h wind, {props.humidity}% humidity and an
@@ -50,6 +81,30 @@ const styles = StyleSheet.create({
     lineHeight: 25,
     textAlign: 'center',
     color: colours.white
+  },
+  iconsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingBottom: 10,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingTop: 5
+  },
+  infoHeading: {
+    color: colours.white,
+    fontFamily: 'allerRg',
+    fontSize: 18,
+    textAlign: 'center'
+  },
+  weatherIconWrapper: {
+    justifyContent: 'center',
+    flexDirection: 'column'
+  },
+  weatherIcon: {
+    color: colours.white,
+    fontSize: 45,
+    textAlign: 'center',
+    fontFamily: 'weatherfont'
   }
 });
 
